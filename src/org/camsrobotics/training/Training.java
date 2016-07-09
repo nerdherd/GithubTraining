0f3e879df12f999faec012e230bee4a3877e27b6
Wesley Soo-Hoo <wesoohoo@gmail.com>
2016-07-09 14:15:50 -0700

    Initial Commit

---
+++ src/org/camsrobotics/training/Training.java
@@ -0,0 +1,10 @@
+package org.camsrobotics.training;
+
+public class Training {
+
+	public static void main(String[] args) {
+		// Have the program greet you by using the System.out.println("string"); command!
+		System.out.println("Hello Wesley!");
+	}
+
+}
