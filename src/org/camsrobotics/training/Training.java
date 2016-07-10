6ab26f1a905dd9b6b709d0c64ee66adc78830f20
Home <Home@10.3.160.58>
2016-07-09 17:56:58 -0700

    hi once more

--- src/org/camsrobotics/training/Training.java
+++ src/org/camsrobotics/training/Training.java
@@ -5,21 +5,17 @@ public class Training {
 	public static void main(String[] args) {
 		// Have the program greet you by using the System.out.println("string"); command!
 		System.out.println("Hello Wesley!");
-
 		System.out.println("Hello Spencer!");
-
 		System.out.println("Hello William");
-
 		System.out.println("Hello Andres!");
 		System.out.println("Hello Wesley Again!");
-
-
 		System.out.println("Hello Spencer!");
 		System.out.println("Hello Jesse!");
 		System.out.println("Hello Wesley Again!");
 		System.out.println("Hello wesley, hows life");
+		System.out.println("How is everyone doing");
+		
 	}
 }
-
 
 
