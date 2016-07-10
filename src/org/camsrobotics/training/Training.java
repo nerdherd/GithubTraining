d6d2265c80e9be7d5a73657a38e43635fc063191
Michael Iovine <mike.iovine7@gmail.com>
2016-07-09 17:12:44 -0700

    training

--- src/org/camsrobotics/training/Training.java
+++ src/org/camsrobotics/training/Training.java
@@ -5,6 +5,7 @@ public class Training {
 	public static void main(String[] args) {
 		// Have the program greet you by using the System.out.println("string"); command!
 		System.out.println("Hello Wesley!");
+		System.out.println("Hello Spencer!");
 	}
 
 }
