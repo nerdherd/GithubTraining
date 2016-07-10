4fcf3faefcf59059a81a3f9c7befbdab146b5e17
Home <Home@10.3.160.58>
2016-07-09 17:41:44 -0700

    hi

--- src/org/camsrobotics/training/Training.java
+++ src/org/camsrobotics/training/Training.java
@@ -6,6 +6,8 @@ public class Training {
 		// Have the program greet you by using the System.out.println("string"); command!
 		System.out.println("Hello Wesley!");
 		System.out.println("Hello Spencer!");
+		System.out.println("Hello William");
 	}
-
 }
+
+
